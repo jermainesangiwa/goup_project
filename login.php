04c97727947d3005d2ff69a5a40f44840f90c186
Jermaine Herman Sangiwa <88526685+jermainesangiwa@users.noreply.github.com>
2025-08-04 10:26:06 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,4 +1,27 @@
 <?php
 // Database configuration, using config.php
-require 'config.php';
+include("config.php");
+
+// Take user to home page if already logged in
+if (isset($_SESSION['user_id'])) 
+{
+    header("Location: home.html");
+    exit();
+}
+
+// Initialize error message
+$error = "";
+
+// Handle form submission
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $input = trim($_POST['email']);
+    $password = $_POST['password'];
+
+    // Match email
+    $stmt = $conn->prepare("SELECT user_id, email, password_hash FROM Users WHERE email = ?");
+    $stmt->bind_param("s", $input);
+    $stmt->execute();
+    $stmt->store_result();
+}
+
 ?>
