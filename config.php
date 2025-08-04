24495937d3e419ea8ad79c435cb654a80b86f671
Jermaine Herman Sangiwa <88526685+jermainesangiwa@users.noreply.github.com>
2025-08-04 10:29:51 +0530

    Update config.php

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
 // Database configuration
-define('DB_HOST', 'jdrive0910-sangiwa-63af.h.aivencloud.com');
-define('DB_USER', 'avnadmin');
-define('DB_PASS', 'AVNS_Em3M-Zft657W02bs9p3');
-define('DB_NAME', 'ailse247');
-define('DB_PORT', '26925'); // Assuming the port is 5432, adjust if necessary
+define('DB_HOST', 'sql306.infinityfree.com');
+define('DB_USER', 'if0_39620755');
+define('DB_PASS', 'Jermaine2000tz');
+define('DB_NAME', 'if0_39620755_ailse247
+');
+define('DB_PORT', '3306'); // Assuming the port is 5432, adjust if necessary
 
 // Create database connection
 $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
