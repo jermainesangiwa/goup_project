50f1640cfe00e5b01a79d3a99f9db367cdd64279
Jermaine Herman Sangiwa <88526685+jermainesangiwa@users.noreply.github.com>
2025-08-20 10:16:50 +0530

    update

--- user_login.php
+++ user_login.php
@@ -57,7 +57,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         body {
             font-family: Arial, Helvetica, sans-serif;
-            background: url('Untitled design (2).png') repeat;
+            background: url('Background_Image_1.png') repeat;
             background-size: 400px auto; /* adjust tile size */
             animation: scrollUp 30s linear infinite;
             color: #ffffff;
