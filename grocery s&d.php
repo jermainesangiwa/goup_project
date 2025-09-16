ae79f010d908fdd8e0b33ad396ec2ec35ce6998c
Jermaine Herman Sangiwa <88526685+jermainesangiwa@users.noreply.github.com>
2025-09-16 12:32:21 +0530

    Update grocery s&d.php

--- grocery s&d.php
+++ grocery s&d.php
@@ -166,11 +166,11 @@
   
    <!-- Categories -->
    <section class="cats container" id="cats">
-    <div class="cat-card active" onclick="window.location.href='grocery fruits.php'">
+    <div class="cat-card" onclick="window.location.href='grocery fruits.php'">
         <i class="material-icons" style="font-size:40px;color:#2b7a78;">emoji_food_beverage</i>
-        <div class="label">Fruits</div>
+        <div class="label">Food & Fruits</div>
     </div>
-    <div class="cat-card" onclick="window.location.href='grocery s&d.php'">
+    <div class="cat-card active" onclick="window.location.href='grocery s&d.php'">
         <i class="material-icons" style="font-size:40px;color:#2b7a78;">local_drink</i>
         <div class="label">Snacks & Drinks</div>
     </div>
